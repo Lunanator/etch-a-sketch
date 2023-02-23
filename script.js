a51172a6d668d48143c6e47e3b5a4f9a6f8731e6
Lunanator <jacobkluna@gmail.com>
2023-02-23 11:01:59 -0800

    Modify the createGrid function to check for already existing pixel elements before creating new ones. If the container is already populated by pixels they are removed prior to generating the new grid

--- script.js
+++ script.js
@@ -4,8 +4,13 @@ container.style.width = '640px';
 container.style.border = '4px solid black';
 
 function createGrid(number) {
+    const elements = document.getElementsByClassName('pixel');
+    while(elements.length > 0){
+        elements[0].parentNode.removeChild(elements[0]);
+    }
     for(let i = 0; i < number * number; i++) {
         const pixel = document.createElement('div');
+        pixel.classList.add('pixel');
         pixel.style.width = (container.clientWidth / number) - 2 + 'px';
         pixel.style.height = (container.clientHeight / number) - 2 + 'px';
         pixel.style.border = '1px solid black';
