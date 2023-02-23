b611bcb6ac001de5e46208b44671c0dfec70cc0c
Lunanator <jacobkluna@gmail.com>
2023-02-23 11:26:57 -0800

    Add event listener to pixels that change their background color to black and border color to white on click.

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ container.style.height = '640px';
 container.style.width = '640px';
 container.style.border = '4px solid black';
 
+
+
 function createGrid(number) {
     const elements = document.getElementsByClassName('pixel');
     while(elements.length > 0){
@@ -11,9 +13,15 @@ function createGrid(number) {
     for(let i = 0; i < number * number; i++) {
         const pixel = document.createElement('div');
         pixel.classList.add('pixel');
+        pixel.setAttribute('id', [i+1])
         pixel.style.width = (container.clientWidth / number) - 2 + 'px';
         pixel.style.height = (container.clientHeight / number) - 2 + 'px';
         pixel.style.border = '1px solid black';
+        pixel.addEventListener("click", () => {
+            pixel.style.backgroundColor = 'black';
+            pixel.style.borderColor = 'white'; 
+        });
         container.append(pixel);
     }
 }
+
