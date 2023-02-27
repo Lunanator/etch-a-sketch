4744e7021283590d346e05a5936c793073e4bc25
Lunanator <jacobkluna@gmail.com>
2023-02-27 10:59:07 -0800

    Add text box to recieve user input for grid dimension. Add button which runs the createGrid function on click event using the number entered in the text box

--- script.js
+++ script.js
@@ -3,6 +3,12 @@ container.style.height = '640px';
 container.style.width = '640px';
 container.style.border = '4px solid black';
 
+const userInput = document.querySelector('#userInput');
+
+const button = document.querySelector('#button');
+button.addEventListener("click", () => {
+    createGrid(userInput.value);
+});
 
 
 function createGrid(number) {
@@ -18,10 +24,18 @@ function createGrid(number) {
         pixel.style.height = (container.clientHeight / number) - 2 + 'px';
         pixel.style.border = '1px solid black';
         pixel.addEventListener("click", () => {
-            pixel.style.backgroundColor = 'black';
-            pixel.style.borderColor = 'white'; 
+            if (pixel.style.backgroundColor = 'white') {
+                pixel.style.backgroundColor = 'black';
+                pixel.style.borderColor = 'white'; 
+            } else if (pixel.style.backgroundColor = 'black') {
+                pixel.style.backgroundColor = 'white';
+                pixel.style.borderColor = 'black'; 
+            }           
         });
         container.append(pixel);
     }
 }
+
+
+
 
