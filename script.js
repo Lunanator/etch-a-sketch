48fdfb68e3b8cae0365e94391012ecb5a2c824c9
Lunanator <jacobkluna@gmail.com>
2023-02-27 14:37:36 -0800

    Add mode buttons to choose between black and white and random colors. Add functions to change the color of pixels on click based on the current mode selection. Black and white switches between black and white on click and random colors changes a pixel to a randomly generated color

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+let colorMode = 0;
+
 const container = document.querySelector('#container');
 container.style.height = '640px';
 container.style.width = '640px';
@@ -5,11 +7,22 @@ container.style.border = '4px solid black';
 
 const userInput = document.querySelector('#userInput');
 
-const button = document.querySelector('#button');
-button.addEventListener("click", () => {
+const buttonOne = document.querySelector('#b1');
+buttonOne.addEventListener("click", () => {
     createGrid(userInput.value);
 });
 
+const blackWhiteButton = document.querySelector('#b2');
+blackWhiteButton.addEventListener("click", () => {
+    colorMode = 1;
+    console.log(colorMode);
+});
+
+const randomColorButton = document.querySelector('#b3');
+randomColorButton.addEventListener("click", () => {
+    colorMode = 2;
+    console.log(colorMode);
+});
 
 function createGrid(number) {
     const elements = document.getElementsByClassName('pixel');
@@ -23,19 +36,27 @@ function createGrid(number) {
         pixel.style.width = (container.clientWidth / number) - 2 + 'px';
         pixel.style.height = (container.clientHeight / number) - 2 + 'px';
         pixel.style.border = '1px solid black';
+        pixel.style.backgroundColor = "white";
         pixel.addEventListener("click", () => {
-            if (pixel.style.backgroundColor = 'white') {
-                pixel.style.backgroundColor = 'black';
-                pixel.style.borderColor = 'white'; 
-            } else if (pixel.style.backgroundColor = 'black') {
-                pixel.style.backgroundColor = 'white';
-                pixel.style.borderColor = 'black'; 
-            }           
+            if (colorMode === 1) {
+                if (pixel.style.backgroundColor === 'white') {
+                    pixel.style.backgroundColor = 'black';
+                    pixel.style.borderColor = 'white'; 
+                } else if (pixel.style.backgroundColor === 'black') {
+                    pixel.style.backgroundColor = 'white';
+                    pixel.style.borderColor = 'black'; 
+                }
+            } else if (colorMode === 2) {
+            const setColor = () => {
+                const randomColor = Math.floor(Math.random()*16777215).toString(16);
+                pixel.style.backgroundColor = `#${randomColor}`;
+                console.log(randomColor);
+                console.log(pixel.style.backgroundColor);
+            }
+            setColor();
+            }
         });
         container.append(pixel);
     }
 }
-
-
-
 
